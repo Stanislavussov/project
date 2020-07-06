6786f8d57511cca9fbebcff9d68c178bd4579b69
Stanislav <example@gmail.com>
2020-07-06 20:45:48 +0200

    OOP prototype Object.create(soldier), Object.setPrototype(soldier, john)

--- js/script.js
+++ js/script.js
@@ -1,83 +1,15 @@
 "use strict";
 
-// function copyFunc (myObj){
-//     const copyObj = {};
+const soldier = {
+    health: 400,
+    armor: 120,
+    sayHello: function(){
+        console.log('Hi from John');
+    }    
+};
 
-//     for (let key in myObj){
-//         copyObj[key] = myObj[key];
-//     }
+const john = Object.create(soldier);
 
-//     return copyObj;
-// }
+// Object.setPrototypeOf(soldier, john);
 
-
-// const numbers = {
-//     a: 2,
-//     b: 5,
-//     c: {
-//         x: 7,
-//         y: 4
-//     }
-// };
-
-// const newNumbers = copyFunc(numbers);
-
-// newNumbers.a = 10;
-
-// console.log(newNumbers);
-
-
-
-// function copy(mainObj){
-//     let objCopy = {};
-
-//     let key;
-//     for (key in mainObj){
-//         objCopy[key] = mainObj[key];
-//     }
-
-//     return objCopy;
-// }
-
-
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-// const add = {
-//     d: 11,
-//     e: 22
-// };
-
-// const together = Object.assign({}, add);
-
-// together.d = 66;
-
-// console.log(add);
-// console.log(together);
-
-
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'abcd';
-
-console.log(newArray);
-
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube', 'myvideo'],
-      blogs = ['wordpress', 'lj', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'fb'];
-
-console.log(internet);
-
-function logg (a, b, c){
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const numb = [2, 3, 5];
-logg(...numb);
+console.log(john.armor);
